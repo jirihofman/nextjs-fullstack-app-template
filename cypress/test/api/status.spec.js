f11d631116552a9445435a0423c0cb4b2b944735
Jiří Hofman <jiri.hofman@gmail.com>
2023-01-10 15:15:35 +0100

    test: fix status test

---
+++ cypress/test/api/status.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Status API', function() {
+
+    beforeEach(function() {
+        // cy.task('db:seed');
+        // Instead the data is seeded in npm run test:db:init
+    });
+
+    it('Reports status', function() {
+        cy.request('GET', '/api/status').then((response) => {
+            expect(response.status).to.eq(200);
+        });
+    });
+});
