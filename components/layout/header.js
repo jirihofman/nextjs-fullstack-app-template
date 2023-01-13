ae274c68700c0881ff04f53c4880c6eb5b3ce028
Jiří Hofman <jiri.hofman@gmail.com>
2023-01-13 14:56:21 +0100

    feat: updated features component (#2)
    * feat: update app name
    * fix: app title, clickable menu item
    * feat: add link to github repo into header

--- components/layout/header.js
+++ components/layout/header.js
@@ -32,7 +32,8 @@ export default function Header() {
 
                     <Navbar.Collapse id="navbarScroll">
                         <Nav className='me-auto'>
-                            <Link passHref href="#"><Nav.Link onClick={() => { alert('I do nothing!'); }}>FAQ</Nav.Link></Link>
+                            <Link passHref href="#"><Nav.Link onClick={() => { alert('I do nothing!'); }}>Menu item</Nav.Link></Link>
+                            <Link passHref href="https://github.com/jirihofman/nextjs-fullstack-app-template"><Nav.Link>GitHub</Nav.Link></Link>
 
                             <NavDropdown title={'Site'}>
                                 <Link passHref href="/faq"><NavDropdown.Item>FAQ</NavDropdown.Item></Link>
