e0d61dabec80699fa96b7147e7d99f9e89d4e841
Jiří Hofman <jiri.hofman@gmail.com>
2023-01-15 21:13:52 +0100

    feat: update features texts and icons

--- next.config.js
+++ next.config.js
@@ -8,11 +8,16 @@ const nextConfig = {
     },
     images: {
         domains: [
-            // Avatar icons from different Next-auth providers.
+            // Avatar icons from different Next-auth providers. Icons for included software.
+            'next-auth.js.org',
+            'github.githubassets.com',
             'avatars.githubusercontent.com',
+            'camo.githubusercontent.com',
+            'raw.githubusercontent.com',
             'lh3.googleusercontent.com',
+            'cdn-icons-png.flaticon.com',
         ],
     },
     reactStrictMode: true,
 };
-module.exports = withTM({ nextConfig });
+module.exports = withTM(nextConfig);
