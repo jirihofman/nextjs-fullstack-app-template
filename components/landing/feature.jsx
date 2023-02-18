c05fc7189f5107a512a09a0b1dfddb339814cef2
Jiří Hofman <jiri.hofman@gmail.com>
2023-02-18 14:03:01 +0100

    chore: codemod next image experimanteal

--- components/landing/feature.jsx
+++ components/landing/feature.jsx
@@ -2,25 +2,35 @@ import { Button, Container } from 'react-bootstrap';
 import { BookHalf } from 'react-bootstrap-icons';
 import useTranslation from 'next-translate/useTranslation';
 import Link from 'next/link';
-import Image from 'next/legacy/image';
+import Image from 'next/image';
 
 const Feature = () => {
     const { t } = useTranslation();
-    const w = '40px';
+    const w = '40';
     const features = [
         {
             buttons: [
                 <Link key={1} passHref href="https://nextjs.org/"><Button className='m-1' variant="primary"><BookHalf className='bi' /> Nextjs</Button></Link>,
             ],
             desc: 'Nextjs framework.',
-            img: <Image alt='' height={w} width={w} src='https://camo.githubusercontent.com/f21f1fa29dfe5e1d0772b0efe2f43eca2f6dc14f2fede8d9cbef4a3a8210c91d/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313636323133303535392f6e6578746a732f49636f6e5f6c696768745f6261636b67726f756e642e706e67' />,
+            img: <Image
+                alt=''
+                height={w}
+                width={w}
+                src='https://camo.githubusercontent.com/f21f1fa29dfe5e1d0772b0efe2f43eca2f6dc14f2fede8d9cbef4a3a8210c91d/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313636323133303535392f6e6578746a732f49636f6e5f6c696768745f6261636b67726f756e642e706e67'
+            />,
             title: 'Nextjs 12.3',
         }, {
             buttons: [
                 <Link key={1} passHref href="https://react-bootstrap.github.io/"><Button className='m-1' variant="primary"><BookHalf className='bi' /> React Bootstrap</Button></Link>,
             ],
             desc: 'Bootstrap 5.',
-            img: <Image alt='' height={w} width={w} src='https://raw.githubusercontent.com/react-bootstrap/react-bootstrap/7c66098610ea7aea89edfe38988990ba8abcd31d/www/static/logo.svg' />,
+            img: <Image
+                alt=''
+                height={w}
+                width={w}
+                src='https://raw.githubusercontent.com/react-bootstrap/react-bootstrap/7c66098610ea7aea89edfe38988990ba8abcd31d/www/static/logo.svg'
+            />,
             title: 'React Bootstrap',
         }, {
             buttons: [
@@ -28,7 +38,12 @@ const Feature = () => {
                 <Link passHref key={1} href={'https://knexjs.org/'}><Button className='m-1' variant="primary" ><BookHalf className='bi' /> Knex</Button></Link>,
             ],
             desc: t('common:feature.desc2'),
-            img: <Image alt='' height={w} width={w} src='https://cdn-icons-png.flaticon.com/512/5968/5968313.png' />,
+            img: <Image
+                alt=''
+                height={w}
+                width={w}
+                src='https://cdn-icons-png.flaticon.com/512/5968/5968313.png'
+            />,
             title: 'MySQL 8.0 + Knex',
         }, {
             buttons: [
@@ -36,28 +51,48 @@ const Feature = () => {
                 <Link passHref key={1} href={'https://jestjs.io/'}><Button className='m-1' variant="primary" ><BookHalf className='bi' /> Jest</Button></Link>,
             ],
             desc: t('common:feature.desc3'),
-            img: <Image alt='' height={w} width={w} src='https://avatars.githubusercontent.com/u/8908513?s=280&v=4' />,
+            img: <Image
+                alt=''
+                height={w}
+                width={w}
+                src='https://avatars.githubusercontent.com/u/8908513?s=280&v=4'
+            />,
             title: 'Cypress and Jest tests',
         }, {
             buttons: [
                 <Link key={1} passHref href="https://docs.github.com/en/actions"><Button className='m-1' variant="primary"><BookHalf className='bi' /> GitHub Actions</Button></Link>,
             ],
             desc: t('common:feature.desc4'),
-            img: <Image alt='' height={w} width={w} src='https://github.githubassets.com/images/modules/site/features/actions-icon-actions.svg' />,
+            img: <Image
+                alt=''
+                height={w}
+                width={w}
+                src='https://github.githubassets.com/images/modules/site/features/actions-icon-actions.svg'
+            />,
             title: 'GitHub Actions',
         }, {
             buttons: [
                 <Link key={1} passHref href="https://next-auth.js.org/"><Button className='m-1' variant="primary"><BookHalf className='bi' /> NextAuth.js</Button></Link>,
             ],
             desc: t('common:feature.desc5'),
-            img: <Image alt='' height={w} width={w} src='https://next-auth.js.org/img/logo/logo-sm.png' />,
+            img: <Image
+                alt=''
+                height={w}
+                width={w}
+                src='https://next-auth.js.org/img/logo/logo-sm.png'
+            />,
             title: 'NextAuth authentication',
         }, {
             buttons: [
                 <Link key={1} passHref href="https://github.com/aralroca/next-translate/"><Button className='m-1' variant="primary"><BookHalf className='bi' /> next-translate</Button></Link>,
             ],
             desc: t('common:feature.desc6', null, { default: 'I18n: 🇨🇿, 🇬🇧' }),
-            img: <Image alt='' height={w} width={w} src='https://github.com/aralroca/next-translate/raw/master/images/logo.svg' />,
+            img: <Image
+                alt=''
+                height={w}
+                width={w}
+                src='https://github.com/aralroca/next-translate/raw/master/images/logo.svg'
+            />,
             title: 'Translations',
         },
     ];
