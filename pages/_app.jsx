836603d0b3097f9b58574438ddc2d1b7ab8d4b62
Jiří Hofman <jiri.hofman@gmail.com>
2023-04-20 21:09:45 +0200

    feat: add Vercel analytics (#16)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,11 +2,14 @@
 import { SessionProvider } from 'next-auth/react';
 import '../style/index.css';
 import { ToastProvider } from '../components/toast-manager';
+import { Analytics } from '@vercel/analytics/react';
+
 export default function MyApp({ Component, pageProps }) {
     return (
         <SessionProvider session={pageProps.session}>
             <ToastProvider>
                 <Component {...pageProps} />
+                <Analytics/>
             </ToastProvider>
         </SessionProvider>
     );
