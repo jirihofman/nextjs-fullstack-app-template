f84bcb4e65ec125273bcbc34d0999cb67f6ac5bd
Jiří Hofman <jiri.hofman@gmail.com>
2023-09-21 12:21:57 +0200

    test: add next-intl provider to component tests

--- components/landing/featureFeature.cy.jsx
+++ components/landing/featureFeature.cy.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Feature from './feature';
+import { NextIntlClientProvider } from 'next-intl';
+import messages from '../../messages/cs.json';
 
 describe('<Feature />', () => {
     it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-        cy.mount(<Feature />);
+        cy.mount(
+            <NextIntlClientProvider locale='cs' messages={messages}>
+                <Feature />
+            </NextIntlClientProvider>
+        );
     });
 });
--- components/layout/footerFooter.cy.js
+++ components/layout/footerFooter.cy.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Footer from './footer';
+import { NextIntlClientProvider } from 'next-intl';
+import messages from '../../messages/cs.json';
 
 describe('<Footer />', () => {
     it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-        cy.mount(<Footer />);
+        // see: https://on.cypress.io/mounting-react
+        cy.mount(
+            <NextIntlClientProvider locale='cs' messages={messages}>
+                <Footer />
+            </NextIntlClientProvider>
+        );
     });
 });
