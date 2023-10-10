b365ea0125ff13cdf7ecf7b2d0e679a9466d0aef
Jiří Hofman <jiri.hofman@gmail.com>
2023-10-10 14:54:38 +0200

    feat: return of react-bootstrap into RSC

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,17 +1,18 @@
-'use client';
-
 import { useTranslations } from 'next-intl';
 import pjson from '../../package.json';
+import { Navbar } from 'react-bootstrap';
 
 export default function Footer() {
     
     const t = useTranslations('Index');
 
     return (
-        <footer className='navbar navbar-light bg-light'>
+        <Navbar className='w-100 justify-content-between'>
             {/* See https://nextjs.org/blog/next-9-4#new-environment-variables-support for NODE_ENV hack */}
             <span className="text-muted" style={{ fontSize: 'small' }}>{pjson.name} ({t('version')}: {[process.env.NEXT_PUBLIC_NODE_ENV || process.env.NODE_ENV, pjson.version].join('-')})</span>
-            {t('footer.developedWithLove')}
-        </footer>
+            <span className="float-end" style={{ fontSize: 'small' }}>
+                {t('footer.developedWithLove')}
+            </span>
+        </Navbar>
     );
 }
